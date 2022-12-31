339a80c0a7a80d314e031b3f1e49a9797f7dc457
Tuan Nguyen <trentontuannguyen@gmail.com>
2022-12-31 15:12:27 -0500

    add a form in the signup component

--- client/src/Components/Signup.js
+++ client/src/Components/Signup.js
@@ -2,8 +2,20 @@ import React from 'react'
 
 const Signup = () => {
   return (
-    <div>Signup</div>
-  )
+    <div id='signup'> 
+        <form >
+        <label>Username</label>
+        <input type="text" /><br/>
+        <label>Password</label>
+        <input type="password" /><br/>
+        <label>Password Confirmation</label>
+        <input type="password" /><br/>
+        <button type="submit">Sign up!</button>
+    </form>
+    <ul>
+    </ul>
+    </div>
+)
 }
 
 export default Signup

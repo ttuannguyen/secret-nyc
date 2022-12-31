8efa60d8d07fb571c0b3f41c46b7f15c738f4541
Tuan Nguyen <trentontuannguyen@gmail.com>
2022-12-31 14:06:46 -0500

    add state variables to user context

--- client/src/context/user.js
+++ client/src/context/user.js
@@ -3,7 +3,10 @@ import React, { useState, useEffect } from 'react';
 const UserContext = React.createContext();
 
 const UserProvider = ({ children }) => {
-    
+    const [user, setUser] = useState({}); 
+    const [loggedIn, setLoggedIn] = useState(false); // false = initial state is not logged in
+    const [secretSpots, setSecretSpots] = useState([]); // all the spots in db
+    const [visits, setVisits] = useState([]);
 
 
     return (

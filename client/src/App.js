d09986c9091ef637f2725702ade3e09b97dd1e51
Tuan Nguyen <trentontuannguyen@gmail.com>
2022-12-31 13:24:19 -0500

    install react router dom and assign routes to components

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from './Components/Navbar';
 import Home from './Components/Home';
 import Signup from './Components/Signup';
@@ -10,10 +11,16 @@ import Login from './Components/Login';
 function App() {
   return (
     <div className="App">
-      <Home />
-      <Navbar />
-      <Signup />
-      <Login />
+      <Router>
+        <Navbar />
+          <h1>Secret NYC</h1>
+          <p>Discover secret spots in NYC like a true New Yorker!</p>
+        <Routes>
+          <Route exact path="/signup" element={ <Signup /> } />
+          <Route exact path="/login" element={ <Login /> } />
+          <Route exact path="/home" element={ <Home /> } />
+        </Routes>
+      </Router>
     </div>
   );
 }

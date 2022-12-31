663415080ae849cf4b0c0c1daa1cb7d29a21ae77
Tuan Nguyen <trentontuannguyen@gmail.com>
2022-12-31 15:14:15 -0500

    add state variables for form submission the the Signup component

--- client/src/Components/Signup.js
+++ client/src/Components/Signup.js
@@ -1,20 +1,31 @@
-import React from 'react'
+import React, { useContext, useState } from 'react';
+import { UserContext } from '../context/user';
+import { useNavigate } from 'react-router-dom';
 
 const Signup = () => {
-  return (
-    <div id='signup'> 
-        <form >
-        <label>Username</label>
-        <input type="text" /><br/>
-        <label>Password</label>
-        <input type="password" /><br/>
-        <label>Password Confirmation</label>
-        <input type="password" /><br/>
-        <button type="submit">Sign up!</button>
-    </form>
-    <ul>
-    </ul>
-    </div>
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [passwordConfirmation, setPasswordConfirmation] = useState('');
+    // const [errorsList, setErrorsList] = useState([]);
+    const { signup } = useContext(UserContext);
+    const navigate = useNavigate();
+
+
+    return (
+        <div id='signup'> 
+            <form >
+            <label>Username</label>
+            <input type="text" /><br/>
+            <label>Password</label>
+            <input type="password" /><br/>
+            <label>Password Confirmation</label>
+            <input type="password" /><br/>
+            <button type="submit">Sign up!</button>
+        </form>
+        <ul>
+        </ul>
+        </div>
 )
 }
 

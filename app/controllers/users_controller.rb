97ada03ac306e027aecef12a435202a7df994c54
Tuan Nguyen <trentontuannguyen@gmail.com>
2022-12-31 13:11:29 -0500

    create a create method and add strong params in the Users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,19 @@ class UsersController < ApplicationController
     def index
         render json: User.all
     end
+
+    def show
+        render json: SecretSpot.all
+    end
+
+    def create
+        #
+    end
+
+
+    private
+    def user_params
+        params.permit(:username, :password, :password_confirmation)
+    end
+
 end
